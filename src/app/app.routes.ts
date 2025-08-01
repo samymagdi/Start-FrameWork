831f25552800198cfd31625878d9b7574fd51ac0
samymagdi <samymagdy1000@gmail.com>
2025-08-01 16:05:11 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './compontents/home/home';
+import { About } from './compontents/about/about';
+import { Contacts } from './compontents/contacts/contacts';
+import { Portfolio } from './compontents/portfolio/portfolio';
+import { Notfound } from './compontents/notfound/notfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Home,title:'home'},
+    {path:'home',component:Home,title:'home'},
+    {path:'about',component:About,title:'about'},
+    {path:'contact',component:Contacts,title:'contact'},
+    {path:'portfolio',component:Portfolio,title:'portfolio'},
+    {path:'**',component:Notfound,title:'404'}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./compontents/navbar/navbar";
+import { Footer } from "./compontents/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
